d798c0566831c02185c8c0b51a11abb534cf8ffa
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-02-20 09:44:48 -0500

    move existing css to styles.scss and remove original css file

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { chessControl } from './control/control'
 import { chessModel   } from './model/model'
 
 import 'normalize.css/normalize.css'
+import './styles/styles.scss'
 
 const live = window.location.search === '?live=true' ? true : false
 const gameData = chessControl.start(chessModel)
