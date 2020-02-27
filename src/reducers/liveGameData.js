08469390955dc434a8afaa1f7257b2866a4fa25f
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-02-27 11:22:32 -0500

    add live game data reducer

---
+++ src/reducers/liveGameData.js
@@ -0,0 +1,34 @@
+//Reducers
+//Live Game Data Reducer
+const liveGameDataReducerDefaultState = {
+    live: false,
+    status: undefined,
+    socket: undefined,
+    color: undefined
+}
+export default (state = liveGameDataReducerDefaultState, action) => {
+switch (action.type) {
+    case 'SET_LIVE_GAME':
+        return {
+            ...state,
+            live: action.live
+        }
+        case 'SET_LIVE_GAME_STATUS':
+            return {
+                ...state,
+                status: action.status
+            }
+        case 'SET_LIVE_GAME_SOCKET':
+            return {
+                ...state,
+                socket: action.socket
+            }
+        case 'SET_LIVE_GAME_COLOR':
+            return {
+                ...state,
+                color: action.color
+            }
+    default:
+        return state
+}
+}
