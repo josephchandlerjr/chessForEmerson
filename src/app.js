3e86e396a47b19e33a5211344309d31c7e9a26e5
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-02-28 11:13:28 -0500

    set Provider from react-redux as route component

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import Chess from './components/Chess'
 import { chessControl } from './control/control'
 import { chessModel   } from './model/model'
 import configureStore from './store/configureStore'
+import { Provider } from 'react-redux'
 
 const store = configureStore()
 console.log(store.getState())
@@ -16,9 +17,11 @@ const live = window.location.search === '?live=true' ? true : false
 const gameData = chessControl.start(chessModel)
 
 
-ReactDOM.render(<Chess 
-                    control={chessControl}
-                    gameData={gameData}
-                    live={live}
-                    />, 
+const jsx = (
+    <Provider store={store}>
+        <Chess control={chessControl} gameData={gameData} live={live} />
+    </Provider>
+)
+
+ReactDOM.render(jsx , 
                     document.getElementById('app'))
