05bbcf3fadb1fcab334705b81ea6c37196546ad2
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-02-28 17:20:36 -0500

    make Rows a stateless functional component

--- src/components/Rows.js
+++ src/components/Rows.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Row from './Row'
 
-export default class Rows extends React.Component {
-    render() {
+const Rows = (props) => {
         return (
             <div>
-                {[8,7,6,5,4,3,2,1].map( (rank) => <Row key={rank} rank={rank} gameData={this.props.gameData}/>)}
-            </div>
-            
+                {[8,7,6,5,4,3,2,1].map( (rank) => <Row key={rank} rank={rank} gameData={props.gameData}/>)}
+            </div>      
         )
-        
-    }
 }
+
+export default Rows
