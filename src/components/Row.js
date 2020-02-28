207e503b21d7a4c75255e8e696c1b7432c3d05e1
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-02-28 17:10:56 -0500

    refactor row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -6,14 +6,11 @@ export default class Row extends React.Component {
     render() {
         return (
             <div className="row">
-                <Square rank={this.props.rank} file={'a'} gameData={this.props.gameData}/>
-                <Square rank={this.props.rank} file={'b'} gameData={this.props.gameData}/>
-                <Square rank={this.props.rank} file={'c'} gameData={this.props.gameData}/>
-                <Square rank={this.props.rank} file={'d'} gameData={this.props.gameData}/>
-                <Square rank={this.props.rank} file={'e'} gameData={this.props.gameData}/>
-                <Square rank={this.props.rank} file={'f'} gameData={this.props.gameData}/>
-                <Square rank={this.props.rank} file={'g'} gameData={this.props.gameData}/>
-                <Square rank={this.props.rank} file={'h'} gameData={this.props.gameData}/>
+            {["a","b","c","d","e","f","g","h"].map( (file) => <Square 
+                                                                   rank={this.props.rank}
+                                                                   file={file} 
+                                                                   gameData={this.props.gameData}
+                                                                   />)}
             </div>
         )
     }
